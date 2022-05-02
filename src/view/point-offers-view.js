d833251363d6b3149285a87dff7cd77e7d62841f
IrinaVerbitskaja <mgmt-m@yandex.ru>
2022-05-02 17:30:28 +0300

    offers отдельно

--- src/view/point-offers-view.js
+++ src/view/point-offers-view.js
@@ -1,17 +1,23 @@
 import {createElement} from '../render';
 
-const createOfferTemlate = () => (
-  `<li class="event__offer">
-      <span class="event__offer-title">Add luggage</span>
+const createOfferTemlate = (offer) => {
+  const {title, price} = offer;
+  return(
+    `<li class="event__offer">
+      <span class="event__offer-title">${title}</span>
       &plus;&euro;&nbsp;
-      <span class="event__offer-price">50</span>
+      <span class="event__offer-price">${price}</span>
     </li>`
-);
+  );};
 
 
 export default class NewOfferView {
+  constructor (offer){
+    this.offer = offer;
+  }
+
   getTemplate() {
-    return createOfferTemlate();
+    return createOfferTemlate(this.offer);
   }
 
   getElement() {
