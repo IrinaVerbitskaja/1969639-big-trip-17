b6202d0102b052aa8b384c5b006f250d6695b7bb
IrinaVerbitskaja <mgmt-m@yandex.ru>
2022-06-03 15:25:04 +0300

    зад 7, ком 7

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import NewFilterView from './view/filter-view';
 import BoardPresenter from './presenter/board-presenter';
 import {render} from './framework/render';
 import PointModel from './model/point-model';
+import FilterModel from './model/filter-model';
 import {generateFilter} from './util/filter-type';
 
 const tripMain = document.querySelector('.trip-main');
 const filterElement = tripMain.querySelector('.trip-controls__filters');
 const sortContentElement = document.querySelector('.trip-events');
 const pointModel = new PointModel();
+const filterModel = new FilterModel();
 const boardPresenter = new BoardPresenter(sortContentElement, pointModel);
 
 const filters = generateFilter(pointModel.point);
