9365034bbcad564fb203c8daede0037f9a78c1e0
IrinaVerbitskaja <mgmt-m@yandex.ru>
2022-06-03 16:05:20 +0300

    зад 7 ком 9

--- src/main.js
+++ src/main.js
@@ -1,27 +1,22 @@
-import NewFilterView from './view/filter-view';
+//import NewFilterView from './view/filter-view';
 import BoardPresenter from './presenter/board-presenter';
-import {render} from './framework/render';
+//import {render} from './framework/render';
+import FilterPresenter from './presenter/filter-presenter';
 import PointModel from './model/point-model';
 import FilterModel from './model/filter-model';
 //import {generateFilter} from './util/filter-type';
-
-const filters = [
-  {
-    type: 'everything',
-    name: 'EVERYTHING',
-    count: 0,
-  },
-];
 
 const tripMain = document.querySelector('.trip-main');
 const filterElement = tripMain.querySelector('.trip-controls__filters');
 const sortContentElement = document.querySelector('.trip-events');
 const pointModel = new PointModel();
 const filterModel = new FilterModel();
-const boardPresenter = new BoardPresenter(sortContentElement, pointModel);
 
+const boardPresenter = new BoardPresenter(sortContentElement, pointModel);
+const filterPresenter = new FilterPresenter(filterElement, filterModel, pointModel);
 //const filters = generateFilter(pointModel.point);
 
-render(new NewFilterView(filters, 'everything'), filterElement);
+//render(new NewFilterView(filters, 'everything'), filterElement);
 
+filterPresenter.init();
 boardPresenter.init();
