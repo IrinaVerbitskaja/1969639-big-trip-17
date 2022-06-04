41b3afe28c0d6d9e73db57e758fb2bb94d447ac9
IrinaVerbitskaja <mgmt-m@yandex.ru>
2022-06-04 16:27:33 +0300

    зад 7

--- src/util/filter-type.js
+++ src/util/filter-type.js
@@ -15,7 +15,7 @@ const FilterType = {
 const filterEvery = (date1, date2, task) => dayjs(date1).isBefore(dayjs()) && dayjs(date2).isAfter(dayjs()) && !task.isArchive;
 const filterSameFuture = (date1) => dayjs(date1).isAfter(dayjs()) || dayjs(date1).isSame(dayjs());
 
-const filter = {
+const filter  = {
   [FilterType.EVERYTHING]: (tasks) => tasks.filter((task) => !task.isArchive),
   [FilterType.FUTURE]: (tasks) => tasks.filter((task) => (filterSameFuture(task.dateFrom) && !task.isArchive) || filterEvery(task.dateFrom, task.dateTo, task)),
   [FilterType.PAST]: (tasks) => tasks.filter((task) => (dayjs(task.dateTo).isBefore(dayjs()) && !task.isArchive) || filterEvery(task.dateFrom, task.dateTo, task)),
