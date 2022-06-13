1cbb6c6b104845e792b9a24670b7db2281d00cd5
IrinaVerbitskaja <mgmt-m@yandex.ru>
2022-06-13 12:50:38 +0300

    7.1 почти

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
-//import NewFilterView from './view/filter-view';
 import FilterPresenter from './presenter/filter-presenter';
 import BoardPresenter from './presenter/board-presenter';
-//import NewFormPointView from './view/add-new-point-view';
-//import {render, RenderPosition, remove} from './framework/render';
 import PointModel from './model/point-model';
 import FilterModel from './model/filter-model';
-//import {generateFilter} from './util/filter-type';
 import {generateOfferType} from './mock/point';
 import {nanoid} from 'nanoid';
 import {getRandomInteger} from './util/random';
@@ -28,24 +24,17 @@ const newPoint =  {
   type:generateOfferType(),
 };
 
-
 const boardPresenter = new BoardPresenter(sortContentElement, pointModel, filterModel);
 const filterPresenter = new FilterPresenter(filterElement, filterModel, pointModel);
-//const filters = generateFilter(pointModel.point);
-
-//render(new NewFilterView(filters, 'everything'), filterElement);
 
 const handleNewTaskFormClose = () => {
   document.querySelector('.trip-main__event-add-btn').disabled = false;
-  //render(new NewFormPointView(newPoint), sortContentElement, RenderPosition.AFTERBEGIN);
 };
 
 const handleNewTaskButtonClick = () => {
-  console.log('кнопка нажата');
   boardPresenter.createTask(handleNewTaskFormClose, newPoint);
   document.querySelector('.trip-main__event-add-btn').disabled = true;
 };
-
 
 document.querySelector('.trip-main__event-add-btn').addEventListener('click', handleNewTaskButtonClick);
 
