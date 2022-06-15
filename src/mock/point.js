976f55637cb0005f03eb2a376c58f8173c339f37
IrinaVerbitskaja <mgmt-m@yandex.ru>
2022-06-15 19:39:51 +0300

    7,1

--- src/mock/point.js
+++ src/mock/point.js
@@ -64,8 +64,8 @@ const offers = [
 
 const generatePoint = () => ({
   basePrice: getRandomInteger(0, 300),
-  dateFrom: `2022-07-0${getRandomInteger(1, 9)}T22:55:56.845Z`,
-  dateTo: `2022-07-${getRandomInteger(10, 30)}T11:22:13.375Z`,
+  dateFrom: `2022-0${getRandomInteger(1, 9)}-0${getRandomInteger(1, 9)}T22:55:56.845Z`,
+  dateTo: `2022-0${getRandomInteger(1, 9)}-${getRandomInteger(10, 30)}T11:22:13.375Z`,
   destination: destination,
   id: nanoid(),
   isFavorite: false,
